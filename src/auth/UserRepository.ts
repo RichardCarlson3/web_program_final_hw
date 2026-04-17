f06e138d608ab3cf1c62d454eb540a6248c1c6ef
richardcarlson3 <richardcarlson517@gmail.com>
2026-04-17 11:15:54 -0400

    finished

--- src/auth/UserRepository.ts
+++ src/auth/UserRepository.ts
@@ -2,6 +2,17 @@ import type { Result } from "../lib/result";
 import type { AuthError } from "./errors";
 import type { IUserRecord } from "./User";
 
+export interface CreateUserInput {
+
+  email: string;
+
+  displayName: string;
+
+  password: string;
+
+}
+
 export interface IUserRepository {
   findByEmail(email: string): Promise<Result<IUserRecord | null, AuthError>>;
+  create(input: CreateUserInput): Promise<Result<IUserRecord, AuthError>>;
 }
